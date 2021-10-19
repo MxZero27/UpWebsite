77a91e5ba5e804c1d1d9d22315d18d594837bd5a
Mansoon 'MxZero' Mannerson <31831086+MxZero27@users.noreply.github.com>
2021-10-19 17:20:35 -0400

    removed secret

--- UpWebsite/settings.py
+++ UpWebsite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-))pawjy$@tem8!2^u#pffbytq$@bo5z08i(=qwq+8foj_q(81a'
+SECRET_KEY = 'NOT TODAY PAL!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
